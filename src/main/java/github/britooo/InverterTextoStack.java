7b596f13a90ebdad0835590a417f7ac6a6f7a0ef
jodroassis <joao.santos006@sptech.school>
2026-04-08 18:47:39 -0300

    add exerc

--- src/main/java/github/britooo/InverterTextoStack.java
+++ src/main/java/github/britooo/InverterTextoStack.java
@@ -8,6 +8,16 @@ public class InverterTextoStack {
 
             Então, não fique pilhado, use a classe Pilha.
         */
-        return null;
+        Pilha pilha = new Pilha(input.length());
+        String palavraInvertida = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            pilha.push(input.charAt(i));
+        }
+
+        for (int j = 0; j < input.length(); j++) {
+            palavraInvertida += pilha.pop();
+        }
+        return palavraInvertida;
     }
 }
